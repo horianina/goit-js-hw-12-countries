40be6d0842ee4ba799e3aea09beab5ba83666234
Anna Horianina <anna.goryanina88@gmail.com>
2021-01-31 22:46:51 +0300

    added error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,15 +3,17 @@ import refs from './refs';
 import { debounce } from "debounce";
 import './pnotify';
 import '@pnotify/core/dist/BrightTheme.css';
-import { defaults } from '@pnotify/core';
+// import { defaults } from '@pnotify/core';
 // import  PNotify from 'node_modules/@pnotify/core/dist/PNotify.js';
   // import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
   // import PNotify from '@pnotify/dist/PNotify.js';
-import animatePnotify from '@pnotify/animate/dist/PNotifyAnimate.js'
-import PNotify from 'pnotify/package.json'
- import '@pnotify/core/dist/Material.css';
+// import animatePnotify from '@pnotify/animate/dist/PNotifyAnimate.js'
+// import PNotify from 'pnotify/package.json'
+//  import '@pnotify/core/dist/Material.css';
+ import {error, success} from '@pnotify/core'
+ import '@pnotify/core/dist/PNotify.css';
 
-// import 'animate.css/animate.min.css';
+// error({title: 'asdasd', delay: 2000}, 200)
 import list_template from '../templates/list_template.hbs';
 import info_template from '../templates/info_template.hbs';
 let search = '';
@@ -31,10 +33,12 @@ function  fetchCountries  (searchQuery)  {
       .then(data => {
         
         if (data.length > 10) {
+          
           // console.log('error');
-          PNotify.error({
+          error({
             text: `Too many matches found. Please enter a more specific query!`,
-          });
+            delay: 2000
+          },200);
           
         }
         if (data.length > 1 && data.length <= 10) {
