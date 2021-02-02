002b5fe4f98b33d937f99bedf14f9a383e295aa4
Anna Horianina <anna.goryanina88@gmail.com>
2021-02-02 11:56:07 +0300

    added target

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,50 +1,57 @@
 import refs from './refs';
-// import { debounce } from 'lodash.debounce';
-import { debounce } from "debounce";
-import './pnotify';
-import '@pnotify/core/dist/BrightTheme.css';
-import {error, success} from '@pnotify/core'
-import '@pnotify/core/dist/PNotify.css';
 
-// error({title: 'asdasd', delay: 2000}, 200)
+import { debounce } from "debounce";
+// import './pnotify';
+import '@pnotify/core/dist/BrightTheme.css';
+import {error} from '@pnotify/core'
+import '@pnotify/core/dist/PNotify.css';
 import list_template from '../templates/list_template.hbs';
 import info_template from '../templates/info_template.hbs';
-let search = '';
+
 const baseUrl = `https://restcountries.eu/rest/v2/name/`;
 
-const forDebounce = require('debounce');
-const onFetchCountries = forDebounce(fetchCountries, 500);
+// const forDebounce = require('debounce');
+const onFetchCountries = debounce(fetchCountries, 500);
 
-function  fetchCountries  (searchQuery)  {
-  search = refs.searchQuery.value;
-
-  if (search) {
-    let url = `${baseUrl}${search}`;
-
+function  fetchCountries  ({target})  {
+   
+    let url = `${baseUrl}${target.value}`;
     fetch(url)
-      .then(res => res.json())
+      .then(res => {
+        if(!res.ok){
+          throw new Error()
+        }
+        return res.json()      
+      })
+        
       .then(data => {
         
         if (data.length > 10) {
           
-          // console.log('error');
+        //  console.log('error');
           error({
             text: `Too many matches found. Please enter a more specific query!`,
             delay: 2000
-          },200);
-          
+          },200); 
         }
         if (data.length > 1 && data.length <= 10) {
           const list = data.map(element => element.name);
           refs.content.innerHTML = list_template(list);
         }
-        if (data.length === 1) {
-          
+        if (data.length === 1) { 
           refs.content.innerHTML = info_template(data[0]);
           console.log(data[0]);
         }
-      });
+      })
+      .catch (e => {
+        error(
+          {
+            text: `Sumething went`,
+            delay: 2000,
+
+          },200
+        )
+      })
   }
-}
 
 refs.searchQuery.addEventListener('input', fetchCountries);
